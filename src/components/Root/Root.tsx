bcb60ee36cad5cb682a64de842d9eecdfbdb355a
fadaeixlii <mohammad4252@gmail.com>
2024-04-23 08:45:56 +0330

    Update

--- src/components/Root/Root.tsx
+++ src/components/Root/Root.tsx
@@ -1,9 +1,9 @@
-import { setDebug } from '@tma.js/sdk';
-import { DisplayGate, SDKProvider, useLaunchParams } from '@tma.js/sdk-react';
-import { TonConnectUIProvider } from '@tonconnect/ui-react';
-import { type FC, useEffect, useMemo } from 'react';
+import { setDebug } from "@tma.js/sdk";
+import { DisplayGate, SDKProvider, useLaunchParams } from "@tma.js/sdk-react";
+import { TonConnectUIProvider } from "@tonconnect/ui-react";
+import { type FC, useEffect, useMemo } from "react";
 
-import { App } from '~/components/App/App.tsx';
+import { App } from "~/components/App/App.tsx";
 
 const Err: FC<{ error: unknown }> = ({ error }) => {
   return (
@@ -11,9 +11,7 @@ const Err: FC<{ error: unknown }> = ({ error }) => {
       <p>An error occurred while initializing the SDK</p>
       <blockquote>
         <code>
-          {error instanceof Error
-            ? error.message
-            : JSON.stringify(error)}
+          {error instanceof Error ? error.message : JSON.stringify(error)}
         </code>
       </blockquote>
     </div>
@@ -21,29 +19,29 @@ const Err: FC<{ error: unknown }> = ({ error }) => {
 };
 
 const Loading: FC = () => {
-  return (
-    <div>Application is loading</div>
-  );
+  return <div>Application is loading</div>;
 };
 
 export const Root: FC = () => {
   const launchParams = useLaunchParams();
 
   const manifestUrl = useMemo(() => {
-    return new URL('tonconnect-manifest.json', window.location.href).toString();
+    return new URL("tonconnect-manifest.json", window.location.href).toString();
   }, []);
 
   // Enable debug mode to see all the methods sent and events received.
   useEffect(() => {
-    if (launchParams.startParam === 'debug') {
+    if (launchParams.startParam === "debug") {
       setDebug(true);
-      import('eruda').then((lib) => lib.default.init());
+      import("eruda").then((lib) => lib.default.init());
     }
   }, [launchParams]);
 
   return (
     <TonConnectUIProvider manifestUrl={manifestUrl}>
-      <SDKProvider options={{ acceptCustomStyles: true, cssVars: true, complete: true }}>
+      <SDKProvider
+        options={{ acceptCustomStyles: true, cssVars: true, complete: true }}
+      >
         <DisplayGate error={Err} loading={Loading} initial={Loading}>
           <App />
         </DisplayGate>
